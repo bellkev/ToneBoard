4886b32d29ccc70e7078d0955dee812db9361221
Kevin Bell <kevin.a.bell@gmail.com>
2021-12-04 21:32:20 -0800

    Initial Commit

---
+++ ToneBoard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToneBoard
+//
+//  Created by Kevin Bell on 12/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToneBoard/ToneBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToneBoardApp.swift
+//  ToneBoard
+//
+//  Created by Kevin Bell on 12/4/21.
+//
+
+import SwiftUI
+
+@main
+struct ToneBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
